ab3e440fd18b70daa4dbfa164225dc13b5407b19
Osman Başar Akkaya <akkayabasar@gmail.com>
2020-08-27 00:38:22 +0300

    Created project & implemented array functions

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "./lib/ArrayFunctions";
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/lib/ArrayFunctions.js
@@ -0,0 +1,28 @@
+Array.prototype.mymap = function (callback) {
+  let newArr = [];
+
+  for (var i = 0; i < this.length; i++) {
+    newArr[i] = callback(this[i], i);
+  }
+  return newArr;
+};
+
+Array.prototype.myfilter = function (callback) {
+  let newArr = [];
+
+  for (var i = 0; i < this.length; i++) {
+    if (callback(this[i])) {
+      newArr.push(this[i]);
+    }
+  }
+
+  return newArr;
+};
+
+Array.prototype.myreduce = function (callback, accumulator) {
+  for (var i = 0; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i]);
+  }
+
+  return accumulator;
+};
